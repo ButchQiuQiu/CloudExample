086722b7f23045b31d222ee00b65534fe08beca1
ButchQiuQiu <133465202@qq.com>
2020-06-10 16:27:54 +0800

    update

--- cloudexample/security-zuul6101/src/main/java/com/butch/securityzuul6101/DemoApplication.java
+++ cloudexample/security-zuul6101/src/main/java/com/butch/securityzuul6101/DemoApplication.java
@@ -2,12 +2,9 @@ package com.butch.securityzuul6101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.butch.apiutils.jwt.JwtServerProperties;
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
