aad74dff561a26bd456b7500bc139faa0d4e3555
ButchQiuQiu <133465202@qq.com>
2020-06-17 14:49:49 +0800

    负载均衡

---
+++ cloudexample/gateway-top6001/src/main/java/com/butch/gatewaytop6001/config/RibbonGlobalLoadBalancingConfiguration.java
@@ -0,0 +1,48 @@
+package com.butch.gatewaytop6001.config;
+
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 全局负载均衡策略:
+ * 
+ * 只能注入一个，
+ * 被ComponentScan扫描到就默认所有服务生效。
+ * 
+ * 如果想要指定服务那么就不能自动注入,得放置在自动扫描外，
+ * 在启动类使用注解@RibbonClient(name = "serviceName", configuration = xxxConfig.class)注入并指定服务.
+ */
+@Configuration
+public class RibbonGlobalLoadBalancingConfiguration {
+    /**
+     * 最低并发
+     */
+    @Bean
+    public IRule bestAvailableRule(){
+        return new BestAvailableRule();
+    }
+
+    //响应时间权重
+    // @Bean
+    public IRule weightedResponseTimeRule(){
+        return new WeightedResponseTimeRule();
+    }
+
+    //轮询
+    // @Bean
+    public IRule roundRobinRule(){
+        return new RoundRobinRule();
+    }
+
+    //随机
+    // @Bean
+    public IRule randomRule(){
+        return new RandomRule();
+    }
+}
