c2e5202ab06ebce447687e2aace983fc88bc32e0
ButchQiuQiu <133465202@qq.com>
2020-07-27 09:38:07 +0800

    update

--- cloudexample/eureka7001/src/test/java/com/butch/eureka7001/DemoApplicationTests.java
+++ cloudexample/eureka7001/src/test/java/com/butch/eureka7001/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Eureka7001Tests {
 
 	@Test
 	void contextLoads() {
